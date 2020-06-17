7fabd83ed2100d2b50fd2555d422f15637492d10
Lasse Haffke <5haffke@informatik.uni-hamburg.de>
2020-06-17 10:56:05 +0200

    Create farmers residence prefab

--- Assets/Scripts/ScriptableObjects/BuildingStats/HousingBuildingStats.cs
+++ Assets/Scripts/ScriptableObjects/BuildingStats/HousingBuildingStats.cs
@@ -5,4 +5,10 @@ public class HousingBuildingStats : GeneralBuildingStats
 {
     [Tooltip("Number of maximal inhabitants")]
     public int MaxInhabitants;
+
+    [Tooltip("Number of workers at the placement of the building")]
+    public int StartingInhabitants;
+
+    [Tooltip("Interval in which new workers are created")]
+    public int WorkerGenerationIntervalInSeconds;
 }
