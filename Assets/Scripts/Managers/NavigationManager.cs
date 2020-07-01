db9fcc7f0e104ab171bd65c6d132239b6702863e
Lasse Haffke <5haffke@informatik.uni-hamburg.de>
2020-07-01 14:39:05 +0200

    Move potentialFields into seperate class PotentialMap

--- Assets/Scripts/Managers/NavigationManager.cs
+++ Assets/Scripts/Managers/NavigationManager.cs
@@ -1,4 +1,5 @@
-﻿using IGDSS20.Buildings;
+﻿using IGDSS20.Assets.Scripts.Navigation;
+using IGDSS20.Buildings;
 using IGDSS20.Enums;
 using System.Collections;
 using System.Collections.Generic;
@@ -6,25 +7,18 @@ using UnityEngine;
 
 public class NavigationManager : MonoBehaviour
 {
-    // String für ugly to string method for potentialfield (just for testing)
-    private string str;
-
-    [Tooltip("This is the weight of the start tile. Assignment says 0.")]
-    public int weightOfStartBuilding = 0;
-
     /// <summary>
     /// Creates a list that contains a tile and its weight that depends on its type & the path to it. 
     /// </summary>
     /// <param name="building">Building to use as starting point for the potential map</param>
-    public List<(Tile,int)> createPotentialFieldMapFor(Building building)
+    public PotentialMap createPotentialFieldMapFor(Building building)
     {
         // add start tile and its weight
         Tile buildingsTile = building.Tile;
-        List <(Tile, int)> potentialFields = new List<(Tile, int)>();
-        potentialFields.Add((buildingsTile, weightOfStartBuilding));
+        var potentialFields = new PotentialMap(buildingsTile);
 
         // start recursion
-        addNeighboringPotentialFields(buildingsTile, weightOfStartBuilding, potentialFields);
+        addNeighboringPotentialFields(buildingsTile, 0, potentialFields);
 
         // return result
         return potentialFields;
@@ -32,58 +26,27 @@ public class NavigationManager : MonoBehaviour
 
     // this is recursive
     // fills the list of potential fields with tiles and their weights. 
-    private void addNeighboringPotentialFields(Tile lastTile, int lastTotalWeight, List<(Tile, int)> potentialfields)
+    private void addNeighboringPotentialFields(Tile lastTile, int lastTotalWeight, PotentialMap potentialfields)
     {
         foreach (Tile tile in lastTile.NeighbouringTiles)
         {
-            // weight of tile =  weight of path + weight from time
             int totalWeight = lastTotalWeight + getWeightForTile(tile);
-            
-            // check for duplicate tiles
-            // choose lowest value
-            if (!potentialfields.Exists(item => item.Item1 == tile))
+
+            if (potentialfields.Add(tile, totalWeight))
             {
-                // add complete value of path
-                potentialfields.Add((tile, totalWeight));
                 addNeighboringPotentialFields(tile, totalWeight, potentialfields);
             }
             else
             {
-                // a value for a path to this tile has been already stored
-                (Tile, int) duplicate = potentialfields.Find(item => item.Item1 == tile);
-                if(duplicate.Item2 > totalWeight)
+                var previousWeight = potentialfields.GetWeight(tile);
+                if (totalWeight < previousWeight)
                 {
-                    // new path to this tile is better than saved one --> replace old value
-                    potentialfields.Remove(duplicate);
-                    potentialfields.Add((tile, totalWeight));
-
+                    potentialfields.UpdateWeight(tile, totalWeight);
                 }
-                //else
-                //{
-                //    // nothing to do because the new path is not better than the known one
-                //}
             }
         }
 
     }
-
-
-    /*
-     * ugly to string method to check saved values in potenfialFields
-     * 
-     * CAN BE DELETED AFTER TESTING
-     */ 
-    private string potentialFieldsToString(List<(Tile, int)> potFileds)
-    {
-        str = "";
-
-        foreach ((Tile, int) item in potFileds){
-            str += $"({item.Item1}, {item.Item2}), \n";
-        }
-
-        return str;
-    }
-
 
     /*
      * Return weight for tile type. 
@@ -91,7 +54,8 @@ public class NavigationManager : MonoBehaviour
      * possible: return also bool that saves, if there has benn an error (default: ...)
      * --> catch in Potential Field Map Generation --> wight of current field is int.Infinite
      */
-    private int getWeightForTile(Tile tile) { 
+    private int getWeightForTile(Tile tile)
+    {
         switch (tile.Type)
         {
             case TileType.Water:
---
+++ Assets/Scripts/Managers/PotentialMap.cs
@@ -0,0 +1,56 @@
+﻿using IGDSS20.Buildings;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine.WSA;
+
+namespace IGDSS20.Assets.Scripts.Navigation
+{
+    public class PotentialMap
+    {
+        private Dictionary<Tile, int> _weightMap;
+        private Tile _startingPoint;
+
+        public PotentialMap(Tile startingPoint)
+        {
+            _weightMap = new Dictionary<Tile, int>();
+            _startingPoint = startingPoint;
+
+            _weightMap.Add(startingPoint, 0);
+        }
+
+        public bool Add(Tile tile, int weight)
+        {
+            bool unknownTile = !_weightMap.ContainsKey(tile);
+
+            if (unknownTile)
+                _weightMap.Add(tile, weight);
+
+            return unknownTile;
+        }
+
+        public void UpdateWeight(Tile tile, int newWeight)
+        {
+            _weightMap[tile] = newWeight;
+        }
+
+        public int GetWeight(Tile tile)
+        {
+            return _weightMap[tile];
+        }
+
+        public override string ToString()
+        {
+            var str = "";
+
+            foreach (var item in _weightMap)
+            {
+                str += $"({item.Key}, {item.Value}), \n";
+            }
+
+            return str;
+        }
+    }
+}
